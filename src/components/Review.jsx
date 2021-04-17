393e4a2c5c1232edde37d48d34e7f0aec3bd5c15
laura-2035 <matiussifa@gmail.com>
2021-04-17 02:17:58 -0300

    HTML and CSS

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import '../components/Card.scss'
 export default props => {
     return (
         <div className="reviews">
-            <img src={props.imgsource}></img>
-            <username>{props.username}</username>
-            <userverified>{props.userverified}</userverified>
-            <blockquote>"{props.review}"</blockquote>
+            <div className="user-info-wrapper">
+                <img src={props.imgsource}></img>
+                <div className="user-info">
+                    <p className="username">{props.username}</p>
+                    <p className="verified">{props.userverified}</p>
+                </div>
+            </div>    
+            <blockquote className="review">" {props.review} "</blockquote>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import './assets/css/utils.scss'
 import Card from './components/Card.jsx'
+
 
 ReactDOM.render(
   <Card></Card>,
